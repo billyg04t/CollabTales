ec5ec730f3d640ef2dd25f4609061588e6ce7dd3
Shaun Swinehart <shaun.leroyhigh@hotmail.com>
2023-12-02 13:21:14 -0600

    reconfigured queries.js to cohere thematically to assignment

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,35 @@
+
 import { gql } from '@apollo/client';
 
-export const QUERY_TECH = gql`
-  query tech {
-    tech {
+// Query for user authentication
+export const AUTH_QUERY = gql`
+  query auth {
+    user {
+      _id
+      username
+    }
+  }
+`;
+
+// Query for fetching story contributors
+export const CONTRIBUTORS_QUERY = gql`
+  query contributors {
+    contributors {
       _id
       name
     }
   }
 `;
 
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
+// Query for fetching recent story updates
+export const CONTRIBUTIONS_QUERY = gql`
+  query contributions {
+    contributions {
       _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+      content
+      createdAt
     }
   }
 `;
+
+// Add more queries related to story matchups, contributions, etc., based on your schema
