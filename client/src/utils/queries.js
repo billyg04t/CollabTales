6d51122be931057ae7b9b434b9025c1ff8a17c51
Shaun Swinehart <shaun.leroyhigh@hotmail.com>
2023-12-02 13:31:37 -0600

    fixed version mistake in previous commit

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,4 @@
+// queries.js
 
 import { gql } from '@apollo/client';
 
@@ -11,23 +12,35 @@ export const AUTH_QUERY = gql`
   }
 `;
 
+// Mutation for creating a new story
+export const CREATE_STORY = gql`
+  mutation createStory($title: String!, $initialContent: String!) {
+    createStory(title: $title, initialContent: $initialContent) {
+      _id
+      title
+    }
+  }
+`;
+
 // Query for fetching story contributors
 export const CONTRIBUTORS_QUERY = gql`
   query contributors {
     contributors {
       _id
       name
+      // You can include additional contributor details here
     }
   }
 `;
 
-// Query for fetching recent story updates
+// Query for fetching recent story contributions
 export const CONTRIBUTIONS_QUERY = gql`
   query contributions {
-    contributions {
+    updates {
       _id
       content
       createdAt
+      // Include other relevant update details
     }
   }
 `;
