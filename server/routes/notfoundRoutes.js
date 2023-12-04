7b78c0a480b86061309194ee02dcb2f0d7023dc5
miekoooo <mocket17@gmail.com>
2023-12-03 18:22:22 -0600

    added vote and notfound routes

---
+++ server/routes/notfoundRoutes.js
@@ -0,0 +1,21 @@
+// App.js or your main routing component
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './client/src/pages/Home';
+import Matchup from './client/src/pages/Matchup';
+import NotFound from './client/src/pages/NotFound';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        
+        {/* Catch-all route for unmatched paths */}
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
---
+++ server/routes/voteRoutes.js
@@ -0,0 +1,22 @@
+// App.js or your main routing component
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './client/src/pages/Home';
+import Vote from './client/src/pages/Vote'; // Adjust the path accordingly
+import NotFound from './client/src/pages/NotFound';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/vote/:id" component={Vote} /> {/* Add this line */}
+        
+        {/* Catch-all route for unmatched paths */}
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
