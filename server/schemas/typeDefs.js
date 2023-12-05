af96bebba77cd14590c14e650b7592aadd90aba8
alondra zurita <alondrazurita2@gmail.com>
2023-12-04 20:49:45 -0600

    added typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,25 +1,44 @@
-const typeDefs = `
-  type Tech {
+//const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type User {
     _id: ID!
-    name: String!
+    username: String!
+    email: String!
+    contributions: [Contribution]
   }
 
-  type Matchup {
+  type Contribution {
     _id: ID!
-    story1: String!
-    story2: String!
-    story1_votes: Int
-    story2_votes: Int
+    user: User
+    story: Story
+    content: String
+    created_at: Date
   }
 
-  type Query {
-    stories: [Stories]
-    matchups(_id: String): [Matchup]
+  type Story {
+    _id: ID!
+    title: String!
+    content: String!
+    genre: String!
+    tags: [ID] # Assuming Tag is a simple ID reference
+    author: User
+    contributions: [Contribution]
+    created_at: Date
   }
 
-  type Mutation {
-    createMatchup(story1: String!, story2: String!): Matchup
-    createVote(_id: String!, storyNum: Int!): Matchup
+  extend type Query {
+    getUser(userId: ID!): User
+    getContribution(contributionId: ID!): Contribution
+    getStory(storyId: ID!): Story
+    # Add other queries as needed
+  }
+
+  extend type Mutation {
+    createUser(username: String!, email: String!, password: String!): User
+    createContribution(userId: ID!, storyId: ID!, content: String!): Contribution
+    createStory(title: String!, content: String!, genre: String!, authorId: ID!): Story
+    # Add other mutations as needed
   }
 `;
 
