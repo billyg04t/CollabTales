63d3899e0efb13fae1f8d601b674781e81f91ea0
Tyler Farrior <tylerfarrior@gmail.com>
2023-12-04 20:16:44 -0500

    fixing bugs with Graphql

--- server/server.js
+++ server/server.js
@@ -1,81 +1,41 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/collabtale', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => console.log('Connected to MongoDB'));
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
-// Define Contribution schema
-const contributionSchema = new mongoose.Schema({
-  text: String,
-  author: String,
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
 
-// Create Contribution model
-const Contribution = mongoose.model('Contribution', contributionSchema);
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  app.use('/graphql', expressMiddleware(server));
 
-// Define GraphQL schema
-const typeDefs = gql`
-  type Contribution {
-    id: ID!
-    text: String!
-    author: String!
-  }
+  // if we're in production, serve client/dist as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  type Query {
-    contributions: [Contribution]
-  }
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  } 
 
-  type Mutation {
-    addContribution(text: String!, author: String!): Contribution
-  }
-`;
-
-// Define GraphQL resolvers
-const resolvers = {
-  Query: {
-    contributions: async () => {
-      try {
-        const contributions = await Contribution.find();
-        return contributions;
-      } catch (error) {
-        console.error('Error fetching contributions:', error.message);
-        throw new Error('Failed to fetch contributions');
-      }
-    },
-  },
-  Mutation: {
-    addContribution: async (_, { text, author }) => {
-      try {
-        const newContribution = new Contribution({ text, author });
-        await newContribution.save();
-        return newContribution;
-      } catch (error) {
-        console.error('Error adding contribution:', error.message);
-        throw new Error('Failed to add contribution');
-      }
-    },
-  },
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
 };
 
-// Create an Apollo Server instance
-const server = new ApolloServer({ typeDefs, resolvers });
-
-// Create Express app
-const app = express();
-
-// Middleware
-app.use(bodyParser.json());
-
-// Apply the Apollo Server middleware to Express
-server.applyMiddleware({ app });
-
-// Start server
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/graphql`);
-});
+startApolloServer();
