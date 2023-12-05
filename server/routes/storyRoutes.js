a91de597c448714e00ddc6dd279112179ba59779
miekoooo <mocket17@gmail.com>
2023-12-04 19:08:37 -0600

    fixed a couple import paths in routes, added userRoutes.js

---
+++ server/routes/storyRoutes.js
@@ -0,0 +1,27 @@
+// App.js or your main routing component
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './client/src/pages/Home';
+import Vote from './client/src/pages/Vote';
+import Story from './client/src/pages/Story'; // Adjust the path accordingly
+import NotFound from './client/src/pages/NotFound';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/vote/:id" component={Vote} />
+        <Route exact path="/matchup" component={CreateContribution} />
+        
+        {/* Route for creating a story */}
+        <Route exact path="/story" component={Story} />
+
+        {/* Catch-all route for unmatched paths */}
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App
