bd2dc4aa40e97bc9d830ea20539467fc11bea162
Shaun Swinehart <shaun.leroyhigh@hotmail.com>
2023-12-06 17:43:14 -0600

    added auth functionality to front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -21,4 +21,16 @@ export const CREATE_STORY = gql`
   }
 `;
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 // Add more mutations as needed for your story assignment
