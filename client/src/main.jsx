3cea283f86bc1fdf31fd0e6828b739cb9a1f7453
Tyler Farrior <tylerfarrior@gmail.com>
2023-12-06 21:46:38 -0500

    added components folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 import Home from './pages/Home.jsx';
 import Story from './pages/Story.jsx';
 import NotFound from './pages/NotFound.jsx';
-import CollabTale from './pages/CollabTale.jsx';
+import CollabTale from './components/CollabTale.jsx';
 
 const router = createBrowserRouter([
   {
