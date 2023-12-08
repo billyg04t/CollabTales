a92210e3748c6df55237759e6ac4cdda24b99420
Shaun Swinehart <shaun.leroyhigh@hotmail.com>
2023-12-07 19:01:47 -0600

    one more syntax error in homeroutes

--- server/routes/homeRoutes.js
+++ server/routes/homeRoutes.js
@@ -2,7 +2,7 @@ const React = require('react');
 const { BrowserRouter, Route, Switch } = require('react-router-dom');
 const Home = require('../../client/src/pages/Home');
 const NotFound = require('../../client/src/pages/NotFound');
-const HomeRoutes = () => {
+const homeRoutes = () => {
   return (
     <Router>
       <Switch>
