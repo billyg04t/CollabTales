0d208c8468f32f7b5fe1b939b150b68ac276e003
Baboon1098 <137217755+Baboon1098@users.noreply.github.com>
2023-12-07 18:47:53 -0600

    Tried to fix Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,29 +1,47 @@
 import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
+
+// Import your GraphQL queries and mutations
+import { GET_RECENT_STORIES, CREATE_STORY_MUTATION } from './graphql'; // Adjust the path accordingly
 
 const Dashboard = () => {
   // Use your query for recent stories
-  const { loading: storiesLoading, data: storiesData } = useQuery(GET_RECENT_STORIES, {
-    fetchPolicy: 'no-cache',
-  });
+  const { loading: storiesLoading, data: storiesData, refetch: refetchRecentStories } = useQuery(
+    GET_RECENT_STORIES,
+    {
+      fetchPolicy: 'no-cache',
+    }
+  );
 
-  const recentStories = storiesData?.recentStories || [];
-
-  // State for creating a new story
   const [newStoryTitle, setNewStoryTitle] = useState('');
+
+  // Use the mutation hook for creating a new story
+  const [createStoryMutation, { loading: createStoryLoading }] = useMutation(
+    CREATE_STORY_MUTATION,
+    {
+      // Optionally, you can update the cache or refetch queries after a successful mutation
+      // update: (cache, { data: { createStory } }) => {
+      //   // Update the cache or refetch queries as needed
+      // },
+    }
+  );
 
   const handleCreateStory = async () => {
     try {
-      // Call your API endpoint to create a new story here
-      // You might need to use a mutation instead of a query for this operation
-      // Example: const response = await createStoryMutation({ variables: { title: newStoryTitle } });
+      const response = await createStoryMutation({
+        variables: { title: newStoryTitle },
+      });
 
-      // After creating the story, you can refetch the recent stories if needed
-      // refetchRecentStories();
 
-      // For simplicity, let's log the new story title for now
-      console.log('New story created:', newStoryTitle);
+      const newStory = response.data.createStory;
+
+      console.log('New story created:', newStory);
+
+      
+      setNewStoryTitle('');
+
+      refetchRecentStories();
     } catch (error) {
       console.error('Error creating a new story:', error);
     }
@@ -40,7 +58,7 @@ const Dashboard = () => {
           <div>Loading stories...</div>
         ) : (
           <ul>
-            {recentStories.map((story) => (
+            {storiesData.recentStories.map((story) => (
               <li key={story._id}>
                 <h3>{story.title}</h3>
                 <p>Author: {story.author.username}</p>
@@ -65,7 +83,9 @@ const Dashboard = () => {
             onChange={(e) => setNewStoryTitle(e.target.value)}
           />
         </label>
-        <button onClick={handleCreateStory}>Create Story</button>
+        <button onClick={handleCreateStory} disabled={createStoryLoading}>
+          {createStoryLoading ? 'Creating...' : 'Create Story'}
+        </button>
       </div>
 
       {/* Links to user profile and liked stories */}
@@ -73,7 +93,6 @@ const Dashboard = () => {
         <h2>User Options:</h2>
         <Link to="/profile">View Your Profile</Link>
         <br />
-        <Link to="/liked-stories">View Liked Stories</Link>
       </div>
     </div>
   );
