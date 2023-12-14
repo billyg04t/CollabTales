73ecdc3f14255a7a26da7bf3a8e833ae861c66fd
miekoooo <mocket17@gmail.com>
2023-12-13 18:24:32 -0600

    got profile page to load

--- client/src/pages/SingleStory.jsx
+++ client/src/pages/SingleStory.jsx
@@ -4,6 +4,7 @@ import { useQuery } from '@apollo/client';
 import ContributionList from '../components/ContributionList';
 import ContributionForm from '../components/ContributionForm';
 import { GET_STORY } from '../utils/queries';
+import "./Page's.css";
 
 const SingleStory = () => {
   const { storyId } = useParams();
@@ -21,12 +22,13 @@ const SingleStory = () => {
     console.error('Error fetching story:', error);
     return <div>Error fetching story</div>;
   }
+
   return (
     <div className="my-3">
       <h3 className="card-header bg-dark text-light p-2 m-0">
-        {story.title} - {story.genre} <br />
+        {story.title} <br />
         <span style={{ fontSize: '1rem' }}>
-          Created this story on {new Date(story.created_at).toLocaleDateString()}
+          Created this story on {new Date(story.createdAt).toLocaleDateString()}
         </span>
       </h3>
       <div className="bg-light py-4">
